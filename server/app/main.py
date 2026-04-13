18e9e40dd229f970f4b9e30c8f6ed04680fcb34e
Christopher Debray <christopherdebray1@gmail.com>
2026-04-13 17:30:22 +0200

    feat: add exception handlers (http, generic, unique constraint)

--- server/app/main.py
+++ server/app/main.py
@@ -1,3 +1,6 @@
+from fastapi import FastAPI, HTTPException
+from app.core.exceptions.unique_constraint_exceptions import UniqueConstraintException
+from app.core.middleware.exception_handler import generic_exception_handler, http_exception_handler, conflict_exception_handler
 from app.core.middleware.response_wrapper import ResponseWrapperMiddleware
 from app.routes._system import router as system_router
 from app.module.user.presentation.routes import router as user_router
@@ -7,6 +10,9 @@ app = FastAPI(
 )
 
 app.add_middleware(ResponseWrapperMiddleware)
+app.add_exception_handler(UniqueConstraintException, conflict_exception_handler)
+app.add_exception_handler(HTTPException, http_exception_handler)
+app.add_exception_handler(Exception, generic_exception_handler)
 
 app.include_router(system_router)
 app.include_router(user_router)
