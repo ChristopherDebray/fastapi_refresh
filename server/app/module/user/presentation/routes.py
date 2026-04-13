8f2b77be84338ee6ab3c2b88d060a309e51f4ed9
Christopher Debray <christopherdebray1@gmail.com>
2026-04-13 15:43:53 +0200

    chore: added bruno api calls for delete / get / update user

--- server/app/module/user/presentation/routes.py
+++ server/app/module/user/presentation/routes.py
@@ -1,15 +1,11 @@
 from fastapi import APIRouter, Depends, status
-from sqlalchemy.orm import Session
-from sqlalchemy import select
 
-from app.db.database import get_db
 from app.module.user.application.use_cases.delete_user_use_case import DeleteUserUseCase
 from app.module.user.application.use_cases.get_user_use_case import GetUserUseCase
 from app.module.user.application.use_cases.get_users_use_case import GetUsersUseCase
 from app.module.user.application.use_cases.update_user_use_case import UpdateUserUseCase
 from app.module.user.infrastructure.dtos.outputs import UserResponseDto
 from app.module.user.infrastructure.dtos.inputs import UserCreateDto, UserUpdateDto
-from app.module.user.infrastructure.persistence.user_model import UserModel
 from app.module.user.application.use_cases.create_user_use_case import CreateUserUseCase
 from app.module.user.presentation.dependancies import get_create_user_use_case, get_delete_user_use_case, get_get_user_use_case, get_get_users_use_case, get_update_user_use_case
 
@@ -24,7 +20,7 @@ def get_user(user_id: int, use_case: GetUserUseCase = Depends(get_get_user_use_c
     return use_case.execute(user_id)
 
 @router.post("", response_model=UserResponseDto, status_code=status.HTTP_201_CREATED)
-def create_user(payload: UserCreateDto, use_case: CreateUserUseCase = Depends(get_create_user_use_case)) -> UserModel: # type: ignore
+def create_user(payload: UserCreateDto, use_case: CreateUserUseCase = Depends(get_create_user_use_case)) -> UserResponseDto:
     return use_case.execute(payload)
 
 @router.patch("/{user_id}", status_code=status.HTTP_200_OK)
@@ -32,7 +28,7 @@ def update_user(
     user_id: int,
     payload: UserUpdateDto,
     use_case: UpdateUserUseCase = Depends(get_update_user_use_case)
-) -> UserModel:
+) -> UserResponseDto:
     return use_case.execute(user_id, payload)
 
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
