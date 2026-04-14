336960dc8882c50e6ca2213022a7a5b5d075b7aa
Christopher Debray <christopherdebray1@gmail.com>
2026-04-14 13:03:38 +0200

    config: Added cors middleware

--- server/app/main.py
+++ server/app/main.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.exceptions.unique_constraint_exceptions import UniqueConstraintException
 from app.core.middleware.exception_handler import generic_exception_handler, http_exception_handler, conflict_exception_handler, validation_exception_handler
 from app.core.middleware.response_wrapper import ResponseWrapperMiddleware
 from app.routes._system import router as system_router
 from app.module.user.presentation.routes import router as user_router
+from app.core.config import settings
 
 app = FastAPI(
     title="Fastapi refresh",
     version="1.0.0",
     openapi_version="3.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allowed_origins_list(),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.add_middleware(ResponseWrapperMiddleware)
